0c0c5db87331508205238de4da23982a9b692d73
Kilpivii <viivi.kilpiainen@gmail.com>
2022-11-23 20:10:34 +0200

    tehtävä 5. osa 5. kaksi eri lisäystä ostoskoriin, hinta on oikein

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -27,3 +27,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(leipa)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korilla_oikea_hinta(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.hinta(), 7)
