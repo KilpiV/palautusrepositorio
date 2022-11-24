23b08f95150c0521b86ad6a42142e7193dbb228e
Kilpivii <viivi.kilpiainen@gmail.com>
2022-11-24 21:50:29 +0200

    tehtävä 5. osa 12. kaksi samaa lisäystä ostoskoriin, ostoksia yksi korissa, jolla oikea nimi ja määrä

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -78,3 +78,12 @@ class TestOstoskori(unittest.TestCase):
         ostos = ostokset[0]
         self.assertEqual(len(ostokset), 1)
         self.assertEqual(ostos.lukumaara(), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
+        ostos = ostokset[0]
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 2)
